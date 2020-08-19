babf09f173cdbdb1499a833d6cf56df2688d937e
Shivani Jadon <shivanijadon98@gmail.com>
2020-08-19 21:15:42 +0530

    configure handlebar

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressHandlebars = require('express-handlebars');
+
+// configuring handlebar as our template engine
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+// request handler
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 // listening to the server at port
 app.listen(port, function(err){
