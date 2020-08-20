5879c3b61c1e995fb547fe9b1b4a4da39fee6ea6
Shivani Jadon <shivanijadon98@gmail.com>
2020-08-21 03:11:02 +0530

    send data to the handlebars view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const dummyData = require('../data/dummyData');
 
 console.log("loading routes");
 
 // request handler
 router.get('/', (req, res) => {
-    res.render('home');
+    console.log(dummyData);
+    res.render('home',{
+        data : dummyData
+    });
 });
 
 module.exports = router;
